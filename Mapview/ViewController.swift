b28777e3a4857c932a0cfa648cffb083e685d130
quy-ld <leuducquy@gmail.com>
2015-12-13 19:22:57 +0700

    second commit

--- Mapview/ViewController.swift
+++ Mapview/ViewController.swift
@@ -14,49 +14,65 @@ class ViewController: UIViewController ,MKMapViewDelegate,CLLocationManagerDeleg
     @IBOutlet weak var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        // nho set  NSLocationAlwaysUsageDescription va NSLocationWhenInUseUsageDescription trong info.plist
         locations.delegate = self;
+        // lay toa do tot nhat
         locations.desiredAccuracy = kCLLocationAccuracyBest;
+        // luon luon request dia diem
         locations.requestAlwaysAuthorization()
+        // bat dau cap nhat dia diem
         locations.startUpdatingLocation()
         let latitude : CLLocationDegrees   = 21.0103460
         let longtitude : CLLocationDegrees    = 105.8364969
+        // xac dinh ti le zoom
         let latDelta : CLLocationDegrees = 0.0005    ;
         let longDelta : CLLocationDegrees  = 0.005;
         let span :MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        // gan vao dia diem
         let location : CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longtitude)
+        // gan vao vung
         let region  : MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        // set vung cho map
         map.setRegion(region, animated: true)
+        // khoi tao cay cot mau do?
         let anotation : MKPointAnnotation   = MKPointAnnotation()
+        // gan toa do cho cay cot
         anotation.coordinate = location
+        // gan tieu de
         anotation.title = "Ha noi"
+        // gan tieu de phu
         anotation.subtitle = "i will be here"
        map.addAnnotation(anotation)
-        let longPress = UITapGestureRecognizer(target: self, action: "action:")
-        longPress.numberOfTapsRequired = 1
-        map.addGestureRecognizer(longPress)
+        // add tap 
+        let tap = UITapGestureRecognizer(target: self, action: "action:")
+        tap.numberOfTapsRequired = 1
+        map.addGestureRecognizer(tap)
         // Do any additional setup after loading the view, typically from a nib.
     }
     func action(longPress : UIGestureRecognizer){
         let latDelta : CLLocationDegrees = 0.0005    ;
         let longDelta : CLLocationDegrees  = 0.005;
         let span :MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
-
+       // lay diem tap
       let location  = longPress.locationInView(self.map)
+        // convert diem tap sang kieu CLLocationCoordinate2D
         let newCoordiante = self.map.convertPoint(location, toCoordinateFromView: self.map)
+        // lay vung tap
         let newregion : MKCoordinateRegion = MKCoordinateRegionMake(newCoordiante, span)
+        // set vung tap cho map
         map.setRegion(newregion, animated: true)
+        
         let newAnotation :MKPointAnnotation = MKPointAnnotation()
         newAnotation.coordinate = newCoordiante
         
         newAnotation.title = "Phu Quoc"
         newAnotation.subtitle = "I will be there"
         map.addAnnotation(newAnotation)
-//        let latitude : CLLocationDegrees   = point.x
-//        let longtitude : CLLocationDegrees    = point.y
 
         print("anotation")
         
     }
+    // update dia diem cua user tren ban do
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let newLocation : CLLocation  = locations[0] 
         let latitude  = newLocation.coordinate.latitude
